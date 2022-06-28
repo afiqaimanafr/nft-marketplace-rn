667422cc8927d91324fccf996b5749b3be573028
Afiq Aiman <afiqaimanafr@Afiqs-MacBook-Pro.local>
2022-06-28 15:27:23 +0800

    update: latest

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,17 @@
 import { useState } from "react";
-import { View, SafeArea, FlatList, Text } from "react-native";
+import { View, SafeAreaView, FlatList, Text, StyleSheet } from "react-native";
 
 import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <FocusedStatusBar background={COLORS.primary} />
+    </SafeAreaView>
+  );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({ container: { flex: 1 } });
